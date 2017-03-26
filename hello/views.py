5b0b72291d0a4ccb623cc5ba287f186c7e74a759
Torrance Yang <torrance.yang@gmail.com>
2017-03-26 18:04:30 -0400

    Added test db files + favicon

--- hello/views.py
+++ hello/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import moviesdb
 
 from .models import Greeting
 
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
+    moviesdb.test()
     return render(request, 'index.html')
 
 
