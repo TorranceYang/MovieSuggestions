ad7e359a6615cb2329882b319765957eb344028d
Torrance Yang <torrance.yang@gmail.com>
2017-03-27 15:08:58 -0400

    Added movie + director collection scraping

---
+++ secondMilestone/createTables.sql
@@ -0,0 +1,32 @@
+CREATE TABLE IF NOT EXISTS Director
+(
+  id INTEGER NOT NULL PRIMARY KEY,
+  first_name VARCHAR(256) NOT NULL,
+  last_name VARCHAR(256) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Movie
+(
+  id INTEGER NOT NULL PRIMARY KEY,
+  title VARCHAR(256) NOT NULL,
+  genre VARCHAR(256) NOT NULL,
+  director INTEGER NOT NULL REFERENCES Director(id),
+  date_released TIMESTAMP NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Rating
+(
+  id INTEGER NOT NULL PRIMARY KEY,
+  source_name VARCHAR(256) NOT NULL,
+  movie_id INTEGER NOT NULL REFERENCES Movie(id),
+  max_rating INTEGER NOT NULL,
+  rating_score FLOAT NOT NULL CHECK(rating_score >= 0 AND rating_score <= max_rating)
+);
+
+--Amount grossed in is millions
+CREATE TABLE IF NOT EXISTS GrossingInfo
+(
+  movie_id INTEGER NOT NULL REFERENCES Movie(id),
+  date_timestamp TIMESTAMP NOT NULL,
+  amount_grossed FLOAT NOT NULL
+);
---
+++ secondMilestone/fillDatabase.py
@@ -0,0 +1,57 @@
+import psycopg2
+from urlparse import urlparse
+import os
+import requests
+import re
+#Run sudo pip install IMDbPY
+from imdb import IMDb
+from datetime import datetime
+
+top250_url = "http://akas.imdb.com/chart/top"
+
+
+#Gets the IMDB Movie ID"s of the top 250 rate movies
+def scrape_top250():
+    r = requests.get(top250_url)
+    html = r.text.split("\n")
+    result = []
+    for line in html:
+        line = line.rstrip("\n")
+        m = re.search(r'data-titleid="tt(\d+?)">', line)
+        if m:
+            _id = m.group(1)
+            result.append(_id)
+    #
+    return result
+
+#Queries imdbpy package for more movie information
+def insert_into_movies_and_directors(top250):
+    result = []
+    ia = IMDb()
+    for movie_id in top250:
+        movie = ia.get_movie(movie_id)
+        director = movie['director'][0]
+        director_name = director["name"].split()
+        first_name = director_name[0]
+        last_name = ' '.join(director_name[1:]).strip()
+        release_date = datetime.strptime(requests.get('http://www.omdbapi.com/?i=tt' + movie_id).json()['Released'], '%d %b %Y')
+        cursor.execute('INSERT INTO Director VALUES (%s, %s, %s) ON CONFLICT DO NOTHING', (director.getID(), first_name, last_name))
+        cursor.execute('INSERT INTO Movie VALUES(%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING', (movie_id, movie['title'], movie['genre'][0], director.getID(), release_date))
+
+def main():
+    print(open("createTables.sql", "r").read())
+    cursor.execute(open("createTables.sql", "r").read())
+    top250 = scrape_top250()
+    insert_into_movies_and_directors(top250)
+
+url = urlparse(os.environ["DATABASE_URL"])
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
+conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+cursor = conn.cursor()
+main()
